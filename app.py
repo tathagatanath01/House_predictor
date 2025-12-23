5801b7f4353f7584421667cb5c2f9fbbbb898b75
Tathagata Nath <iamriju53@gmail.com>
2025-12-24 01:57:51 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+# pickle-mixin, scikit-learn,pandas, numpy, flask
+from flask import Flask, render_template, request
+import pandas as pd
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Read CSV once
+data = pd.read_csv("Cleaned_data.csv")
+pipe = pickle.load(open('RidgeModel.pkl','rb'))
+
+@app.route("/")
+def index():
+    locations = sorted(data["location"].dropna().unique())
+    return render_template("index.html", locations=locations)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    location = request.form.get('location')
+    bhk = float(request.form.get('bhk'))
+    bath = float(request.form.get('bath'))
+    sqft = request.form.get('total_sqft')
+
+    print(location, bhk, bath, sqft)
+    input = pd.DataFrame([[location, sqft, bhk, bath]], columns=['location', 'total_sqft', 'bath', 'bhk'])
+    prediction = pipe.predict(input)[0] *1e5
+
+    return str(np.round(prediction, 2))
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
+
